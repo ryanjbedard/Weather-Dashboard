42e9bc30ec2e8d64c73203abf3e360ea06340d3f
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2023-11-28 22:24:58 -0800

    Start function for pulling in user input and apply to API fetch requests.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,34 @@
-// var cityCoordinates = 
-var cityStateCountryInput = [];
+var userCitySearch = document.querySelector("#user-search");
 var apiKey = "8e727616e135223bd7c755a1def0cd46";
-var requestCoordinates = "'http://api.openweathermap.org/geo/1.0/direct?q=' + cityStateCountryInput + '&limit=5&appid=' + apiKey"
-var request5DayForecast = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
-var test = "http://api.openweathermap.org/geo/1.0/direct?q=Philadelphia&limit=5&appid=8e727616e135223bd7c755a1def0cd46"
+// var requestCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityStateCountryInput + "&limit=5&appid=" + apiKey;
+// var request5DayForecast = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
+// Function taking the user search information from the input field
+function userSearchInput(event) {
+  event.preventDefault();
+  var requestCoordinates =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityStateCountryInput +
+    "&limit=5&appid=" +
+    apiKey;
+  var request5DayForecast =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" + {lat} + "&lon=" + {lon} + "&appid=" + apiKey;
+  var cityStateCountryInput = userCitySearch.value.trim();
+  
+  if(cityStateCountryInput) {
+    userCitySearch.value = "";
+  } else {
+    alert('Please enter a city.');
+  }
 
-fetch(requestCoordinates)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    
-  });
+}
+
+// fetch(requestCoordinates)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+
+//   });
+userSearchInput();
