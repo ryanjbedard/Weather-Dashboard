c487703b7fea50bb05c8129a6ea5139d8c26892f
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2023-11-29 22:38:58 -0800

    Add cards for populating forecast.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,34 +1,47 @@
-var userCitySearch = document.querySelector("#user-search");
+var userCitySearchEl = document.querySelector("#user-search");
 var apiKey = "8e727616e135223bd7c755a1def0cd46";
+var searchHistory = localStorage.getItem("localCity") || []
+var savedCities = JSON.parse(localStorage.getItem("savedCities"))
 // var requestCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityStateCountryInput + "&limit=5&appid=" + apiKey;
 // var request5DayForecast = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
 // Function taking the user search information from the input field
 function userSearchInput(event) {
   event.preventDefault();
+
+ 
+  var cityStateCountryInput = userCitySearchEl.value.trim();
+
+  if (cityStateCountryInput) {
+    getCityCoordinates(cityStateCountryInput);
+
+    userCitySearchEl.value = " ";
+  } else {
+    alert("Please enter a city.");
+  }
+}
+
+var getCityCoordinates = function (cityStateCountryInput) {
   var requestCoordinates =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     cityStateCountryInput +
     "&limit=5&appid=" +
     apiKey;
-  var request5DayForecast =
-    "https://api.openweathermap.org/data/2.5/forecast?lat=" + {lat} + "&lon=" + {lon} + "&appid=" + apiKey;
-  var cityStateCountryInput = userCitySearch.value.trim();
-  
-  if(cityStateCountryInput) {
-    userCitySearch.value = "";
-  } else {
-    alert('Please enter a city.');
-  }
 
+  fetch(requestCoordinates)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+};
+
+function getForecase(lat, lon){
+   var request5DayForecast =
+      "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 }
-
-// fetch(requestCoordinates)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-
-//   });
-userSearchInput();
+console.log(getCityCoordinates);
+userCitySearchEl.addEventListener("click", function(){
+  searchHistory.push[cityInput.value]
+});
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Weather-Dashboard
+# <Your-Project-Title>
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+
+![alt text](assets/images/screenshot.png)
+
+https://ryanjbedard.github.io/Weather-Dashboard/
+
+## Credits
+
+N/A
+
+## License
+
+This application uses a standard MIT license.
+
+---
