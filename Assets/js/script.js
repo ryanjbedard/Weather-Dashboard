625417400148ea72da3363cdf8bc3725b3a76faf
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2023-11-30 18:09:26 -0800

    Adjust JavaScript to correctly in order to pull coordinates from weather API data.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,47 +1,65 @@
-var userCitySearchEl = document.querySelector("#user-search");
+var userCitySearchEl = document.getElementById("searchBox");
+var citySearchButton = document.getElementById("search-button");
+
 var apiKey = "8e727616e135223bd7c755a1def0cd46";
-var searchHistory = localStorage.getItem("localCity") || []
-var savedCities = JSON.parse(localStorage.getItem("savedCities"))
+var searchHistory = localStorage.getItem("localCity") || [];
+var savedCities = JSON.parse(localStorage.getItem("savedCities"));
+
 // var requestCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityStateCountryInput + "&limit=5&appid=" + apiKey;
 // var request5DayForecast = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
 // Function taking the user search information from the input field
-function userSearchInput(event) {
+console.log(citySearchButton);
+
+// Function to Grab API Data
+function getCityData(event) {
+  // Stop Submit so it's not oversubimitting API calls
   event.preventDefault();
 
- 
+  // Assigning variable for city input
   var cityStateCountryInput = userCitySearchEl.value.trim();
 
+  // Checking city input
   if (cityStateCountryInput) {
-    getCityCoordinates(cityStateCountryInput);
-
-    userCitySearchEl.value = " ";
+    getCoordinates(cityStateCountryInput);
+    // Clears Search Box
+    userCitySearchEl.value = "";
   } else {
     alert("Please enter a city.");
   }
 }
 
-var getCityCoordinates = function (cityStateCountryInput) {
+function getCoordinates(city){
+
   var requestCoordinates =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityStateCountryInput +
-    "&limit=5&appid=" +
+      "http://api.openweathermap.org/geo/1.0/direct?q=" +
+      city +
+      "&limit=5&appid=" +
+      apiKey;
+
+     fetch(requestCoordinates)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data[0]);
+        let lat = data[0].lat;
+        let lon = data[0].lon;
+
+        getForecase(lat, lon);
+      });
+
+}
+
+function getForecase(lat, lon) {
+  var request5DayForecast =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
     apiKey;
 
-  fetch(requestCoordinates)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-};
-
-function getForecase(lat, lon){
-   var request5DayForecast =
-      "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    console.log(request5DayForecast)
 }
-console.log(getCityCoordinates);
-userCitySearchEl.addEventListener("click", function(){
-  searchHistory.push[cityInput.value]
-});
+citySearchButton.addEventListener("click", getCityData);
